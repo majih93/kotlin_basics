2673df463ffc3144c09676c7f1a417c8d973ff65
majih93 <t90laser93@gmail.com>
2024-06-30 22:41:08 +0900

    for..in, until, 0..4

--- app/src/main/java/com/udemy_android/kotlinbasics/Basics.kt
+++ app/src/main/java/com/udemy_android/kotlinbasics/Basics.kt
@@ -21,4 +21,23 @@ fun main() {
     println(mutableShoppingList)
 
     // list에는 string, int값들이나, 객체 자체도 저장이 가능하다.
+
+    // 반복문
+    // 코틀린 문법은 for...in 이구만.
+    for(item in mutableShoppingList) {
+        println(item)
+    }
+
+    // JavaScript for문처럼 인덱스 값에 접근하려면?
+    // 0 until list.size 형식으로 작성하는구나. 오홍 꽤나 직관적으로 처리하려고 신경을 좀 많이 쓴 느낌이네.
+    // until은 시작점 포함, 종료점 직전까지
+    for(itemIndex in 0 until mutableShoppingList.size) {
+        println(itemIndex) // 0 1 2 3 4 5
+    }
+
+    // 그러면 종료지점까지 포함시키고 싶으면?
+    for(num in 0..4) {
+        println(num) // 0 1 2 3 4
+    }
+
 }
